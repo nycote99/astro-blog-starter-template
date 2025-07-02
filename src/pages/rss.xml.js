ce34ce80f6c2486b901b9788db2276fb6c24628f
Nicolas <145159785+nycote99@users.noreply.github.com>
2025-07-01 20:31:04 -0400

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,7 +6,7 @@ export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
     title: NicolasYCote,
-    description: DESC,
+    description: Description,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
